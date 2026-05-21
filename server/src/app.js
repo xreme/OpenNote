c06cc6ff42ef677f47bf5d339339e296ab41c192
osereme <reme.ibazebo@gmail.com>
2026-05-21 15:09:16 -0400

    Enable trust proxy for reverse proxy deployments

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,8 @@ if (!fs.existsSync(COLLECTIONS_DIR)) fs.mkdirSync(COLLECTIONS_DIR, { recursive: 
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
